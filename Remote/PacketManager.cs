8c598195650a72d42c6842a0c471eb1fd0bb3ea5
tomes11 <tomes11@users.noreply.github.com>
2016-05-08 14:01:37 +0200

    Added Read/Write methods to PacketManager

--- Remote/PacketManager.cs
+++ Remote/PacketManager.cs
@@ -63,6 +63,145 @@ namespace Remote
 				return i;
 			}
 		}
+		#region Write
+		public void Write(bool value)
+		{
+			bw.Write(value);
+		}
+		public void Write(byte value)
+		{
+			bw.Write(value);
+		}
+		public void Write(byte[] buffer)
+		{
+			bw.Write(buffer);
+		}
+		public void Write(char ch)
+		{
+			bw.Write(ch);
+		}
+		public void Write(char[] chars)
+		{
+			bw.Write(chars);
+		}
+		public void Write(decimal value)
+		{
+			bw.Write(value);
+		}
+		public void Write(double value)
+		{
+			bw.Write(value);
+		}
+		public void Write(float value)
+		{
+			bw.Write(value);
+		}
+		public void Write(int value)
+		{
+			bw.Write(value);
+		}
+		public void Write(long value)
+		{
+			bw.Write(value);
+		}
+		public void Write(sbyte value)
+		{
+			bw.Write(value);
+		}
+		public void Write(short value)
+		{
+			bw.Write(value);
+		}
+		public void Write(string value)
+		{
+			bw.Write(value);
+		}
+		public void Write(uint value)
+		{
+			bw.Write(value);
+		}
+		public void Write(ulong value)
+		{
+			bw.Write(value);
+		}
+		public void Write(ushort value)
+		{
+			bw.Write(value);
+		}
+		public void Write(byte[] buffer, int index, int count)
+		{
+			bw.Write(buffer, index, count);
+		}
+		public void Write(char[] chars, int index, int count)
+		{
+			bw.Write(chars, index, count);
+		}
+		#endregion
+		#region Read
+		public bool ReadBoolean()
+		{
+			return br.ReadBoolean();
+		}
+		public byte ReadByte()
+		{
+			return br.ReadByte();
+		}
+		public byte[] ReadBytes(int count)
+		{
+			return br.ReadBytes(count);
+		}
+		public char ReadChar()
+		{
+			return br.ReadChar();
+		}
+		public char[] ReadChars(int count)
+		{
+			return br.ReadChars(count);
+		}
+		public decimal ReadDecimal()
+		{
+			return br.ReadDecimal();
+		}
+		public double ReadDouble()
+		{
+			return br.ReadDouble();
+		}
+		public short ReadInt16()
+		{
+			return br.ReadInt16();
+		}
+		public int ReadInt32()
+		{
+			return br.ReadInt32();
+		}
+		public long ReadInt64()
+		{
+			return br.ReadInt64();
+		}
+		public sbyte ReadSByte()
+		{
+			return br.ReadSByte();
+		}
+		public float ReadSingle()
+		{
+			return br.ReadSingle();
+		}
+		public string ReadString()
+		{
+			return br.ReadString();
+		}
+		public ushort ReadUInt16()
+		{
+			return br.ReadUInt16();
+		}
+		public uint ReadUInt16()
+		{
+			return br.ReadUInt32();
+		}
+		public ulong ReadUInt16()
+		{
+			return br.ReadUInt64();
+		}
+		#endregion
 	}
-
 }
